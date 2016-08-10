b7d86b9b27ca3cc695803c3768a8aa7803c1f9de
Muriel Silveira <murielkong@gmail.com>
2016-08-10 18:48:55 -0300

    improve algorithm

--- project-euler/07_10001st_prime.js
+++ project-euler/07_10001st_prime.js
@@ -4,7 +4,10 @@ function isPrime(number) {
   if (number < 2)
     return false
 
-  for (let i = 2; i <= Math.sqrt(number); i++) {
+  if (number % 2 === 0)
+    return number === 2
+
+  for (let i = 3; i <= Math.sqrt(number); i += 2) {
     if (number % i === 0)
       return false
   }
