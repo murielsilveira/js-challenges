9c5514656c5563af4811858c105700553ee26320
Muriel Silveira <murielkong@gmail.com>
2016-08-10 18:20:20 -0300

    is prime first version

---
+++ project-euler/07_10001st_prime.js
@@ -0,0 +1,21 @@
+'use strict'
+
+function isPrime(number) {
+  if (number < 2)
+    return false
+
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0)
+      return false
+  }
+  return true
+}
+
+function getNthPrime(nth) {
+
+}
+
+module.exports = {
+  isPrime,
+  getNthPrime
+}
---
+++ project-euler/07_10001st_prime_test.js
@@ -0,0 +1,48 @@
+const assert = require('chai').assert
+const {isPrime, getNthPrime} = require('./07_10001st_prime')
+
+/*
+10001st prime
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+*/
+
+describe('isPrime', function() {
+  it('1 is not prime', function() {
+    assert.isFalse(isPrime(1))
+  })
+
+  it('even greater than 2 are not prime', function() {
+    assert.isFalse(isPrime(4))
+    assert.isFalse(isPrime(10))
+    assert.isFalse(isPrime(1234))
+    assert.isFalse(isPrime(123412340))
+  })
+
+  it('2 is prime', function() {
+    assert.isTrue(isPrime(2))
+  })
+
+  it('3 is prime', function() {
+    assert.isTrue(isPrime(3))
+  })
+
+  it('5 is prime', function() {
+    assert.isTrue(isPrime(5))
+  })
+
+  it('7 is prime', function() {
+    assert.isTrue(isPrime(7))
+  })
+
+  it('13 is prime', function() {
+    assert.isTrue(isPrime(13))
+  })
+
+  it('9007199254740881 is prime', function() {
+    this.timeout(10000)
+    assert.isTrue(isPrime(9007199254740881))
+  })
+})
