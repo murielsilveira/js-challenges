b1fd9c6be53582355bdb39138296f0f39613bde1
Muriel Silveira <murielkong@gmail.com>
2016-08-10 18:51:37 -0300

    apparently this change make the algorithm four times faster

--- project-euler/07_10001st_prime.js
+++ project-euler/07_10001st_prime.js
@@ -7,7 +7,7 @@ function isPrime(number) {
   if (number % 2 === 0)
     return number === 2
 
-  for (let i = 3; i <= Math.sqrt(number); i += 2) {
+  for (let i = 3; i <= Math.sqrt(number); i = i + 2) {
     if (number % i === 0)
       return false
   }
